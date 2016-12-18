0a95b7fd5e14aac31e6427667e95b1c10456b838
mohayonao <mohayonao@gmail.com>
2016-12-19 06:36:20 +0900

    1.1.1

--- build/pluck-string-node.js
+++ build/pluck-string-node.js
@@ -1,13 +1,9 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.PluckStringNode = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
 /* global Float32Array, Symbol, AudioNode, OscillatorNode */
 
-var WS_CURVE_SIZE = 255;
-var curve = new Float32Array(WS_CURVE_SIZE);
-
-for (var i = 0; i < WS_CURVE_SIZE; i++) {
-  var x = i - (WS_CURVE_SIZE >> 1);
-  curve[i] = Math.sin(19 * Math.sin(97 * x) + 47 * Math.sin(31 * Math.sin(23 * x)));
-}
+var ua = (global.navigator && global.navigator.userAgent.toLowerCase()) || "";
+var isSafari = ua.indexOf("safari") !== -1 && ua.indexOf("chrome") === -1;
 
 function PluckStringNode(audioContext, opts) {
   opts = opts || {};
@@ -17,6 +13,7 @@ function PluckStringNode(audioContext, opts) {
   var biquadFilter = audioContext.createBiquadFilter();
   var color = typeof opts.color === "number" ? opts.color : 800;
   var timeConstant = typeof opts.timeConstant === "number" ? opts.timeConstant : 5;
+  var curve = createWaveShaperCurve(255);
 
   oscillator.type = "triangle";
   oscillator.connect(pluckShaper);
@@ -82,7 +79,24 @@ if (typeof Symbol === "function" && typeof Symbol.hasInstance === "symbol") {
   });
 }
 
+function createWaveShaperCurve(length) {
+  var offset = isSafari ? 1 : 0;
+  var curve = new Float32Array(length + offset);
+
+  for (var i = 0; i < length; i++) {
+    curve[i + offset] = Math.random() * 2 - 1;
+  }
+  curve[(length >> 1) + offset] = 0;
+
+  if (offset === 1) {
+    curve[0] = curve[1];
+  }
+
+  return curve;
+}
+
 module.exports = PluckStringNode;
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[1])(1)
 });
